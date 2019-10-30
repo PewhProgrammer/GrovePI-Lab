906c8a043dce90fd46ba9c0d667d6e5afbad3742
Thinh Tran <vlukiray@yahoo.com>
2019-10-30 11:46:25 +0100

    init air quality

---
+++ grove_air_quality.py
@@ -0,0 +1,37 @@
+import math
+import sys
+import time
+from grove.adc import ADC
+
+class GroveAirQualitySensor:
+
+    def __init__(self, channel):
+        self.channel = channel
+        self.adc = ADC()
+
+    @property
+    def value(self):
+        return self.adc.read(self.channel)
+
+Grove = GroveAirQualitySensor
+
+
+def main():
+    if len(sys.argv) < 2:
+        print('Usage: {} adc_channel'.format(sys.argv[0]))
+        sys.exit(1)
+
+    sensor = GroveAirQualitySensor(int(sys.argv[1]))
+
+    print('Detecting ...') 
+    while True:
+        value = sensor.value        
+        if value > 100:
+            print("{}, High Pollution.".format(value))
+        else:
+            print("{}, Air Quality OK.".format(value))
+
+        time.sleep(.1)
+
+if __name__ == '__main__':
+    main()
